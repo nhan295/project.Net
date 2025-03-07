5dfa3abf8d93104a3d2b1a12e85c5baf638833dc
nhan295 <120718344+nhan295@users.noreply.github.com>
2025-03-07 17:33:51 +0700

    Movie Detail page

---
+++ Project.NET/Project.NET/MovieDetail.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Project.NET
+{
+    partial class MovieDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.qu_nh_p_tr_ng___payoff_poster___kc_07032025_1_;
+            pictureBox2.Location = new Point(12, 101);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(130, 170);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.hq720;
+            pictureBox1.Location = new Point(-3, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(566, 179);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 294);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(545, 108);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.Menu;
+            textBox2.Location = new Point(157, 194);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(203, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkRed;
+            button1.Font = new Font("Segoe UI Variable Display", 14F, FontStyle.Bold);
+            button1.ForeColor = SystemColors.Window;
+            button1.Location = new Point(167, 585);
+            button1.Name = "button1";
+            button1.Size = new Size(231, 63);
+            button1.TabIndex = 5;
+            button1.Text = "Book Now";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.Menu;
+            textBox3.Location = new Point(157, 246);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(77, 27);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.Menu;
+            textBox4.Location = new Point(283, 246);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(77, 27);
+            textBox4.TabIndex = 7;
+            // 
+            // MovieDetail
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(562, 675);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "MovieDetail";
+            Text = "MovieDetail";
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private TextBox textBox3;
+        private TextBox textBox4;
+    }
+}
