35cd64b64113aafc3f67d2e50b53feda00a2699f
nhan295 <120718344+nhan295@users.noreply.github.com>
2025-03-12 14:30:26 +0700

    homepage

--- Project.NET/Project.NET/HomePage.Designer.cs
+++ Project.NET/Project.NET/HomePage.Designer.cs
@@ -29,121 +29,366 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            menuStrip1 = new MenuStrip();
-            menuToolStripMenuItem = new ToolStripMenuItem();
-            lichChieuToolStripMenuItem = new ToolStripMenuItem();
-            abcToolStripMenuItem = new ToolStripMenuItem();
-            panelBanner = new Panel();
-            pictureBoxBanner = new PictureBox();
-            flowLayoutPanelMovies = new FlowLayoutPanel();
-            timer2 = new System.Windows.Forms.Timer(components);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePage));
             label1 = new Label();
-            menuStrip1.SuspendLayout();
-            panelBanner.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBoxBanner).BeginInit();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            panel1 = new Panel();
+            panel7 = new Panel();
+            button3 = new Button();
+            pictureBox2 = new PictureBox();
+            panel8 = new Panel();
+            pictureBox8 = new PictureBox();
+            button5 = new Button();
+            panel9 = new Panel();
+            pictureBox7 = new PictureBox();
+            button6 = new Button();
+            panel3 = new Panel();
+            pictureBox6 = new PictureBox();
+            button2 = new Button();
+            panel6 = new Panel();
+            pictureBox5 = new PictureBox();
+            textBox1 = new TextBox();
+            pictureBox3 = new PictureBox();
+            label3 = new Label();
+            pictureBox4 = new PictureBox();
+            sidebarTransition = new System.Windows.Forms.Timer(components);
+            btn_menu = new PictureBox();
+            sidebar = new FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel8.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            panel9.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btn_menu).BeginInit();
+            sidebar.SuspendLayout();
             SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            menuStrip1.BackColor = Color.DarkGray;
-            menuStrip1.ImageScalingSize = new Size(20, 20);
-            menuStrip1.Items.AddRange(new ToolStripItem[] { menuToolStripMenuItem });
-            menuStrip1.Location = new Point(0, 0);
-            menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(562, 28);
-            menuStrip1.TabIndex = 0;
-            menuStrip1.Text = "menuStrip1";
-            // 
-            // menuToolStripMenuItem
-            // 
-            menuToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { lichChieuToolStripMenuItem, abcToolStripMenuItem });
-            menuToolStripMenuItem.Name = "menuToolStripMenuItem";
-            menuToolStripMenuItem.Size = new Size(60, 24);
-            menuToolStripMenuItem.Text = "Menu";
-            // 
-            // lichChieuToolStripMenuItem
-            // 
-            lichChieuToolStripMenuItem.Name = "lichChieuToolStripMenuItem";
-            lichChieuToolStripMenuItem.Size = new Size(159, 26);
-            lichChieuToolStripMenuItem.Text = "Lich Chieu";
-            // 
-            // abcToolStripMenuItem
-            // 
-            abcToolStripMenuItem.Name = "abcToolStripMenuItem";
-            abcToolStripMenuItem.Size = new Size(159, 26);
-            abcToolStripMenuItem.Text = "Abc";
-            // 
-            // panelBanner
-            // 
-            panelBanner.Controls.Add(pictureBoxBanner);
-            panelBanner.Location = new Point(26, 41);
-            panelBanner.Name = "panelBanner";
-            panelBanner.Size = new Size(512, 150);
-            panelBanner.TabIndex = 1;
-            // 
-            // pictureBoxBanner
-            // 
-            pictureBoxBanner.Dock = DockStyle.Fill;
-            pictureBoxBanner.Image = Properties.Resources.cinema_hall_white_blank_screen_600nw_2469487437;
-            pictureBoxBanner.Location = new Point(0, 0);
-            pictureBoxBanner.Name = "pictureBoxBanner";
-            pictureBoxBanner.Size = new Size(512, 150);
-            pictureBoxBanner.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBoxBanner.TabIndex = 0;
-            pictureBoxBanner.TabStop = false;
-            // 
-            // flowLayoutPanelMovies
-            // 
-            flowLayoutPanelMovies.AutoScroll = true;
-            flowLayoutPanelMovies.Location = new Point(0, 244);
-            flowLayoutPanelMovies.Name = "flowLayoutPanelMovies";
-            flowLayoutPanelMovies.Size = new Size(562, 402);
-            flowLayoutPanelMovies.TabIndex = 2;
-            // 
-            // timer2
-            // 
-            timer2.Interval = 3000;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(26, 206);
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(146, 208);
             label1.Name = "label1";
-            label1.Size = new Size(150, 20);
-            label1.TabIndex = 3;
-            label1.Text = "Phim Sex Dang Chieu";
+            label1.Size = new Size(172, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Now Showing";
             label1.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.Window;
+            pictureBox1.Dock = DockStyle.Top;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(859, 46);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(438, 208);
+            label2.Name = "label2";
+            label2.Size = new Size(168, 32);
+            label2.TabIndex = 2;
+            label2.Text = "Coming Soon";
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(12, 255);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(695, 378);
+            panel1.TabIndex = 3;
+            // 
+            // panel7
+            // 
+            panel7.Controls.Add(button3);
+            panel7.Controls.Add(pictureBox2);
+            panel7.Location = new Point(3, 197);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(223, 41);
+            panel7.TabIndex = 9;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.InactiveCaptionText;
+            button3.ForeColor = SystemColors.ButtonFace;
+            button3.Location = new Point(46, -12);
+            button3.Name = "button3";
+            button3.Size = new Size(183, 71);
+            button3.TabIndex = 7;
+            button3.Text = "Log Out";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(0, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(40, 41);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // panel8
+            // 
+            panel8.Controls.Add(pictureBox8);
+            panel8.Controls.Add(button5);
+            panel8.Location = new Point(3, 56);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(223, 41);
+            panel8.TabIndex = 9;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = Properties.Resources.user__1_;
+            pictureBox8.Location = new Point(0, 3);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(40, 41);
+            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox8.TabIndex = 8;
+            pictureBox8.TabStop = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.InactiveCaptionText;
+            button5.ForeColor = SystemColors.ButtonFace;
+            button5.Location = new Point(46, -12);
+            button5.Name = "button5";
+            button5.Size = new Size(183, 71);
+            button5.TabIndex = 7;
+            button5.Text = "Account Information";
+            button5.TextAlign = ContentAlignment.MiddleLeft;
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // panel9
+            // 
+            panel9.Controls.Add(pictureBox7);
+            panel9.Controls.Add(button6);
+            panel9.Location = new Point(3, 150);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(223, 41);
+            panel9.TabIndex = 9;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = Properties.Resources.ticket;
+            pictureBox7.Location = new Point(0, 0);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(40, 36);
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox7.TabIndex = 10;
+            pictureBox7.TabStop = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.InactiveCaptionText;
+            button6.ForeColor = SystemColors.ButtonFace;
+            button6.Location = new Point(46, -12);
+            button6.Name = "button6";
+            button6.Size = new Size(183, 71);
+            button6.TabIndex = 7;
+            button6.Text = "My Tickets";
+            button6.TextAlign = ContentAlignment.MiddleLeft;
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(pictureBox6);
+            panel3.Controls.Add(button2);
+            panel3.Location = new Point(3, 103);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(223, 41);
+            panel3.TabIndex = 8;
+            panel3.Paint += panel3_Paint;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(0, 0);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(40, 41);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 9;
+            pictureBox6.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.InactiveCaptionText;
+            button2.ForeColor = SystemColors.ButtonFace;
+            button2.Location = new Point(46, -12);
+            button2.Name = "button2";
+            button2.Size = new Size(183, 71);
+            button2.TabIndex = 7;
+            button2.Text = "Setting";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // panel6
+            // 
+            panel6.Controls.Add(pictureBox5);
+            panel6.Controls.Add(textBox1);
+            panel6.Location = new Point(3, 3);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(227, 47);
+            panel6.TabIndex = 10;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.dinosaur1;
+            pictureBox5.Location = new Point(3, 6);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(37, 41);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 11;
+            pictureBox5.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(40, 17);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(170, 27);
+            textBox1.TabIndex = 9;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(146, 52);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(460, 105);
+            pictureBox3.TabIndex = 5;
+            pictureBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            label3.Location = new Point(295, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(130, 35);
+            label3.TabIndex = 0;
+            label3.Text = "CINEMAX";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.notification;
+            pictureBox4.Location = new Point(570, 4);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(36, 35);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 0;
+            pictureBox4.TabStop = false;
+            // 
+            // sidebarTransition
+            // 
+            sidebarTransition.Interval = 10;
+            sidebarTransition.Tick += sidebarTransition_Tick;
+            // 
+            // btn_menu
+            // 
+            btn_menu.Image = Properties.Resources.menu1;
+            btn_menu.Location = new Point(12, 12);
+            btn_menu.Name = "btn_menu";
+            btn_menu.Size = new Size(38, 30);
+            btn_menu.SizeMode = PictureBoxSizeMode.StretchImage;
+            btn_menu.TabIndex = 6;
+            btn_menu.TabStop = false;
+            btn_menu.Click += btn_menu_Click;
+            // 
+            // sidebar
+            // 
+            sidebar.BackColor = SystemColors.ActiveCaptionText;
+            sidebar.Controls.Add(panel6);
+            sidebar.Controls.Add(panel8);
+            sidebar.Controls.Add(panel3);
+            sidebar.Controls.Add(panel9);
+            sidebar.Controls.Add(panel7);
+            sidebar.Dock = DockStyle.Left;
+            sidebar.Location = new Point(0, 46);
+            sidebar.Name = "sidebar";
+            sidebar.Size = new Size(10, 597);
+            sidebar.TabIndex = 7;
             // 
             // HomePage
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(562, 675);
+            AutoScroll = true;
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(859, 643);
+            Controls.Add(sidebar);
+            Controls.Add(btn_menu);
+            Controls.Add(pictureBox4);
+            Controls.Add(label3);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel1);
+            Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(flowLayoutPanelMovies);
-            Controls.Add(panelBanner);
-            Controls.Add(menuStrip1);
-            MainMenuStrip = menuStrip1;
+            Font = new Font("Segoe UI", 9F);
             Name = "HomePage";
             Text = "HomePage";
-            menuStrip1.ResumeLayout(false);
-            menuStrip1.PerformLayout();
-            panelBanner.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pictureBoxBanner).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel7.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel8.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            panel9.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btn_menu).EndInit();
+            sidebar.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private MenuStrip menuStrip1;
-        private ToolStripMenuItem menuToolStripMenuItem;
-        private ToolStripMenuItem lichChieuToolStripMenuItem;
-        private ToolStripMenuItem abcToolStripMenuItem;
-        private Panel panelBanner;
-        private PictureBox pictureBoxBanner;
-        private FlowLayoutPanel flowLayoutPanelMovies;
-        private System.Windows.Forms.Timer timer2;
         private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Panel panel1;
+        private Label label3;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private System.Windows.Forms.Timer sidebarTransition;
+        private PictureBox pictureBox2;
+        private PictureBox btn_menu;
+        private FlowLayoutPanel sidebar;
+        private Panel panel3;
+        private Panel panel6;
+        private TextBox textBox1;
+        private PictureBox pictureBox5;
+        private Button button2;
+        private PictureBox pictureBox6;
+        private Panel panel7;
+        private Button button3;
+        private Panel panel8;
+        private Button button5;
+        private Panel panel9;
+        private Button button6;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox7;
     }
 }
--- Project.NET/Project.NET/HomePage.cs
+++ Project.NET/Project.NET/HomePage.cs
@@ -12,58 +12,67 @@ namespace Project.NET
 {
     public partial class HomePage : Form
     {
-        private string[] imagePaths = {
-            @"D:\GitHub\project.Net\Project.NET\Project.NET\Resources\phone-call.png",
-            @"D:\GitHub\project.Net\Project.NET\Project.NET\Resources\padlock.png",
-            @"D:\GitHub\project.Net\Project.NET\Project.NET\Resources\images.jpg"
-        };
-        private int currentIndex = 0;
         public HomePage()
         {
             InitializeComponent();
-            LoadMovieGrid();
-            pictureBoxBanner.ImageLocation = imagePaths[currentIndex]; // Hiển thị ảnh đầu tiên
-            timer2.Tick += TimerSlide_Tick;
-            timer2.Start();
-        }
-        private void TimerSlide_Tick(object sender, EventArgs e)
-        {
-            currentIndex = (currentIndex + 1) % imagePaths.Length; // Chuyển ảnh tiếp theo
-            pictureBoxBanner.ImageLocation = imagePaths[currentIndex];
-        }
-        private void LoadMovieGrid()
-        {
-            string[] imagePaths = {
-                @"D:\GitHub\project.Net\Project.NET\Project.NET\Resources\cinema-hall-white-blank-screen-600nw-2469487437.jpg",
-                @"D:\GitHub\project.Net\Project.NET\Project.NET\Resources\cinema-hall-white-blank-screen-600nw-2469487437.jpg",
-                @"D:\GitHub\project.Net\Project.NET\Project.NET\Resources\cinema-hall-white-blank-screen-600nw-2469487437.jpg",
-                @"D:\GitHub\project.Net\Project.NET\Project.NET\Resources\cinema-hall-white-blank-screen-600nw-2469487437.jpg",
-                @"D:\GitHub\project.Net\Project.NET\Project.NET\Resources\cinema-hall-white-blank-screen-600nw-2469487437.jpg",
-                @"D:\GitHub\project.Net\Project.NET\Project.NET\Resources\cinema-hall-white-blank-screen-600nw-2469487437.jpg",
-                @"D:\GitHub\project.Net\Project.NET\Project.NET\Resources\cinema-hall-white-blank-screen-600nw-2469487437.jpg",
-                @"D:\GitHub\project.Net\Project.NET\Project.NET\Resources\cinema-hall-white-blank-screen-600nw-2469487437.jpg",
-                @"D:\GitHub\project.Net\Project.NET\Project.NET\Resources\cinema-hall-white-blank-screen-600nw-2469487437.jpg"
-            };
-
-            foreach (string path in imagePaths)
-            {
-                PictureBox pic = new PictureBox
-                {
-                    ImageLocation = path,
-                    SizeMode = PictureBoxSizeMode.StretchImage,
-                    Width = 150,
-                    Height = 200,
-                    Margin = new Padding(10) // Tạo khoảng cách giữa các ảnh
-                };
-                flowLayoutPanelMovies.Controls.Add(pic); // Thêm vào FlowLayoutPanel
-            }
-        }
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        bool sidebarExpand = true;
+        private void sidebarTransition_Tick(object sender, EventArgs e)
+        {
+            if (sidebarExpand)
+            {
+                sidebar.Width -= 10;
+                if (sidebar.Width <= 0)
+                {
+                    sidebarExpand = false;
+                    sidebarTransition.Stop();
+                }
+            }
+            else
+            {
+                sidebar.Width += 10;
+                if (sidebar.Width >= 217)
+                {
+                    sidebarExpand = true;
+                    sidebarTransition.Stop();
+                }
+
+            }
+        }
+
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_menu_Click(object sender, EventArgs e)
+        {
+            sidebarTransition.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
         {
 
         }
