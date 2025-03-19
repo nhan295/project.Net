c152f5786967a797883bf4f6092a532a6bddc22f
nhan295 <120718344+nhan295@users.noreply.github.com>
2025-03-19 16:25:05 +0700

    update

---
+++ Project.NET/Project.NET/SeatPage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project.NET
+{
+    public partial class SeatPage : Form
+    {
+        public SeatPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SeatPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox8_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox7_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
